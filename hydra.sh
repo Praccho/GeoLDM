1f816c3a41265902a8baddf7eeba4b5509bb623a
mindy-kim <mindy_kim@brown.edu>
2024-05-04 15:29:26 -0400

    update hydra

--- hydra.sh
+++ hydra.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt install python3-pip
-sudo pip3 uninstall -r requirements.txt
-sudo pip3 uninstall satlaspretrain-models
+sudo pip3 install -r requirements.txt
+sudo pip3 install satlaspretrain-models
 python3 data/satlas_model.py
