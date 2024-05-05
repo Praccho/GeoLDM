bc957cd992bf1d74402d2f9aa1b853bce6ec51b0
mindy-kim <mindy_kim@brown.edu>
2024-05-05 17:35:00 -0400

    mask reshape

--- ldm/models/unet.py
+++ ldm/models/unet.py
@@ -268,13 +268,3 @@ class GroupNorm32(nn.GroupNorm):
     def forward(self, x):
         return super().forward(x.float()).type(x.dtype)
     
-def timestep_embedding(timesteps, dim, max_period=10000):
-    freqs = torch.exp(
-        -math.log(max_period) * torch.arange(start=0, end=dim // 2, dtype=torch.float32) / (dim // 2)
-    ).to(device=timesteps.device)
-    args = timesteps[:, None].float() * freqs[None]
-    embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
-    if dim % 2:
-        embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
-
-    return embedding
