3d4d8cc198cd6404a88f7363e82b3fcf2c2bd407
mindy-kim <mindy_kim@brown.edu>
2024-05-05 17:41:10 -0400

    yo

--- ldm/models/ddpm.py
+++ ldm/models/ddpm.py
@@ -173,7 +173,7 @@ class LatentDiffusion(pl.LightningModule):
 
         if self.training:
             mask = torch.rand(len(batch)) < self.p_uncond
-            mask = torch.reshape(x.shape[0], *((1,) * (len(x.shape) - 1)))
+            mask = torch.reshape((z.shape[0], *((1,) * (len(z.shape) - 1))))
             sat_emb[mask] = 0
             lat_emb[mask] = 0
             lng_emb[mask] = 0
