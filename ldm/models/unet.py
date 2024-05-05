d41945cd6817051709596c126376ee88409d08f5
Praccho Muna-McQuay <praccho_muna-mcquay@brown.edu>
2024-05-05 02:32:21 -0400

    check

--- ldm/models/unet.py
+++ ldm/models/unet.py
@@ -26,7 +26,6 @@ class UNetModel(nn.Module):
             dims=2,
             num_classes=None,
             use_checkpoint=False,
-            use_fp16=False,
             num_heads=-1,
             num_head_channels=-1,
             num_heads_upsample=-1,
@@ -59,7 +58,7 @@ class UNetModel(nn.Module):
             self.conv_resample = conv_resample
             self.num_classes = num_classes
             self.use_checkpoint = use_checkpoint
-            self.dtype = torch.float16 if use_fp16 else torch.float32
+            self.dtype = torch.float16
             self.num_heads = num_heads
             self.num_head_channels = num_head_channels
             self.num_heads_upsample = num_heads_upsample
