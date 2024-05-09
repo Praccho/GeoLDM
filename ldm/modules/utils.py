b6edcedb751b1be106657c196f59e3bbb9494092
Praccho Muna-McQuay <praccho_muna-mcquay@brown.edu>
2024-05-09 16:31:00 -0400

    disabled train

--- ldm/modules/utils.py
+++ ldm/modules/utils.py
@@ -22,3 +22,7 @@ def extract_into_tensor(a, t, x_shape):
     b, *_ = t.shape
     out = a.gather(-1, t)
     return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+
+
+def disabled_train(self, mode=True):
+    return self
