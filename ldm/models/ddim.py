1d2f983b73f59baee341ae15e3018ab4b357993f
Praccho Muna-McQuay <praccho_muna-mcquay@brown.edu>
2024-05-09 17:36:18 -0400

    ddim fixes

--- ldm/models/ddim.py
+++ ldm/models/ddim.py
@@ -29,7 +29,7 @@ class DDIMWrapper(object):
         self.register_buffer('sqrt_inv_alphas_cumprod', to_torch(np.sqrt(1. / alphas_cumprod.cpu())))
         self.register_buffer('sqrt_recipm1_alphas_cumprod', to_torch(np.sqrt(1. / alphas_cumprod.cpu() - 1.)))
 
-        ddim_alphas = alphas_cumprod.cpu()[self.ddim_steps]
+        ddim_alphas = alphas_cumprod.cpu()[self.ddim_timesteps]
         ddim_alphas_prev = np.asarray([alphas_cumprod.cpu()[0]] + alphas_cumprod.cpu()[self.ddim_timesteps[:-1]].tolist())
         
         self.register_buffer('ddim_alphas', ddim_alphas)
@@ -52,9 +52,9 @@ class DDIMWrapper(object):
         alphas_prev = self.ddim_alphas_prev
         sqrt_sub_alphas = self.ddim_sqrt_sub_alphas_cumprod
         
-        a_t = torch.full((bs,1,1,1), alphas[idx], device=device)
-        a_prev = torch.full((bs,1,1,1), alphas_prev[idx], device=device)
-        sqrt_sub_at = torch.full((bs,1,1,1), sqrt_sub_alphas[idx], device=device)
+        a_t = torch.full((x.shape[0],1,1,1), alphas[idx], device=device)
+        a_prev = torch.full((x.shape[0],1,1,1), alphas_prev[idx], device=device)
+        sqrt_sub_at = torch.full((x.shape[0],1,1,1), sqrt_sub_alphas[idx], device=device)
 
         pred_x0 = (x - sqrt_sub_at * eps_pred) / a_t.sqrt()
         dir_xt = (1. - a_prev).sqrt() * eps_pred
@@ -65,25 +65,25 @@ class DDIMWrapper(object):
 
     @torch.no_grad()
     def sample(self, ddim_num_steps, ctx, batch_sz, shape, cfg_scale = None):
+        _, C, H, W = shape
+        shape = (batch_sz, C, H, W)
         self.register_schedule(ddim_num_steps)
 
-        bs = batch_sz if cfg_scale else batch_sz * 2
+        bs = batch_sz * 2 if cfg_scale else batch_sz
 
         if cfg_scale:
             uncond_ctx = torch.zeros_like(ctx, dtype=torch.float32, device = self.model.device)
             ctx = torch.cat([ctx, uncond_ctx])
         
-        C, H, W = shape
-        shape = (bs, C, H, W)
 
         imgs = torch.randn(shape, dtype=torch.float32, device=self.model.device)
-        time_range = tqdm(np.flip(self.ddim_timesteps), desc='DDIM sampling', total=self.ddpm_num_timesteps)
+        time_range = tqdm(np.flip(self.ddim_timesteps), desc='DDIM sampling', total=self.ddim_num_timesteps)
 
         for i, t in enumerate(time_range):
-            if self.use_cfg:
+            if cfg_scale:
                 imgs = imgs.repeat(2,1,1,1)
             idx = len(self.ddim_timesteps) - i - 1
-            ts = torch.fill((bs,), t, device=self.device, dtype=torch.long)
+            ts = torch.full((bs,), int(t), device=self.model.device, dtype=torch.long)
 
             imgs = self.p_sample(imgs, ctx, ts, idx, cfg_scale = cfg_scale)
             
